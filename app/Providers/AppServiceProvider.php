7893bb95b9067a167475536183fa9522dc647a82
Olalekan ISMAIL <irolalekan@gmail.com>
2020-08-25 12:09:36 +0100

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Providers;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('GuzzleHttp\Client', function($api) use ($baseUrl) {
+            return new Client([
+                'base_uri' => $baseUrl,
+            ]);
+        });
+    }
+}
